de0c3ed10fd7b49d0e20b4c56258bbd7da266a36
ansh <ansh.mulchandani01@estudiant.upf.edu>
2026-04-02 20:17:41 +0200

    movement added

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float moveX;
+    private float moveY;
+    public float speed = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
     void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        moveX = movementVector.x;
+        moveY = movementVector.y;
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveX, 0.0f, moveY);   
+        rb.AddForce(movement*speed);
     }
 }
