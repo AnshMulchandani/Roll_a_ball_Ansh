ece4be37b20c839da02b91113ad3f9a1c3598e01
ansh <ansh.mulchandani01@estudiant.upf.edu>
2026-04-02 14:11:50 +0200

    basic scene created, not movement yet

---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    void OnMove(InputValue movementValue)
+    {
+        
+    }
+}
